1d76a316bdafb776d23eafecdd53fd446e098a87
Alex Kost <alexkost819@gmail.com>
2025-07-19 22:37:05 -0700

    Add dependabot, minor tweaks

--- lint.sh
+++ lint.sh
@@ -6,6 +6,6 @@ set -ex  # Exit on error and print commands
 
 uv run --only-group lint typos
 uv run --only-group lint ruff format
-uv run --only-group lint docformatter --in-place src/ tests/ *.py
+uv run --only-group lint docformatter --in-place src/ tests/
 uv run --only-group lint ruff check --fix
 uv run --only-group lint mypy
